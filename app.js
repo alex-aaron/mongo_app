d5675140bad417495513a0f577334c22da7f9d69
alex-aaron <alexfrancisaaron@gmail.com>
2023-05-18 11:52:11 -0500

    created app.js and required express

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+
+
+
+// // requires mongoose package
+// const mongoose = require('mongoose');
+
+// // connects to mongoose test server
+// mongoose.connect('mongodb://localhost:27017/');
